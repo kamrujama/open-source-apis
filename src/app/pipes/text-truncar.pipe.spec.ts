adfff3aa22e039336fdfffe2f29ea878063be95d
ansari <ansaris500@gmail.com>
2024-10-16 23:21:33 +0545

    Cleanup the repository

--- src/app/pipes/text-truncar.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextTruncarPipe } from './text-truncar.pipe';
-
-describe('TextTruncarPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TextTruncarPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
